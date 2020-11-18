1b3505ad08659470dd8904053ada8ba55c7ef6af
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-11-19 01:09:24 +0530

    Add project files.

---
+++ BatteryAnalyserApp/Controllers/BatteryAnalyserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BatteryAnalyserApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatteryAnalyserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetFaultyDevices()
+        {
+            var data = new List<string>() { "sachin"};
+            return data.ToArray();
+        }
+    }
+}
