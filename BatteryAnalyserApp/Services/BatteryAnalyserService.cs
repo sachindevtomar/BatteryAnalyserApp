2fe0a601b14cace397cfb26ceaa968c63047d893
Sachin Dev Tomar <tomarsachindev@gmail.com>
2020-11-19 02:44:56 +0530

    Added code to group the data based on serialNumber

--- BatteryAnalyserApp/Services/BatteryAnalyserService.cs
+++ BatteryAnalyserApp/Services/BatteryAnalyserService.cs
@@ -14,15 +14,37 @@ namespace BatteryAnalyserApp.Services
 
         public List<string> GetDevicesWithFaultyBatteries()
         {
-            List<BatteryData> result;
+            List<BatteryData> batteryData;
+            List<string> faultySerialNumbers = new List<string>();
+            //Create WebClient for downloading the battery data from Github
             using (WebClient wc = new WebClient())
             {
                 var batteryJSON = wc.DownloadString(Constants.BatteryJSONDownloadURL);
-                result = JsonConvert.DeserializeObject<List<BatteryData>>(batteryJSON);
+                batteryData = JsonConvert.DeserializeObject<List<BatteryData>>(batteryJSON);
+
+                //Group data based on SerialNumber
+                var groupedBatteryData = from bd in batteryData group bd by bd.serialNumber;
+
+                //iterate each group        
+                foreach (var serialNumberGroup in groupedBatteryData)
+                {
+                    Console.WriteLine("Age Group: "+serialNumberGroup.Key+" : " + serialNumberGroup.Count()); //Each group has a key 
+
+                    //Check if the device is faulty if yes then insert it into the Final Faulty List
+                    if (IsFaultySerialNumberGroup(serialNumberGroup.ToList()))
+                        faultySerialNumbers.Add(serialNumberGroup.Key);
+                    //foreach (Student s in ageGroup) // Each group has inner collection
+                    //    Console.WriteLine("Student Name: {0}", s.StudentName);
+                }
             }
 
             var data = new List<string>() { "sachin" };
             return data;
+        }
+
+        private bool IsFaultySerialNumberGroup(List<BatteryData> list)
+        {
+            throw new NotImplementedException();
         }
     }
 }
